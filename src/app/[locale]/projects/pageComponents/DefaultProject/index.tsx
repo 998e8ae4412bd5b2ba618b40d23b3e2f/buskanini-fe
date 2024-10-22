7f6175f4e08cff212415a7205d674c6f77aee8b3
ihorvyt <ihorvyt@gmail.com>
2024-10-22 18:12:49 +0300

    made projects page mobile version

--- src/app/[locale]/projects/pageComponents/DefaultProject/index.tsx
+++ src/app/[locale]/projects/pageComponents/DefaultProject/index.tsx
@@ -3,13 +3,14 @@ import styles from "./defultProject.module.scss";
 import PreviewButton from "@/app/components/previewButton";
 
 type CardProject = {
+	name: string
 	image: string;
 };
-const Index = ({ image }: CardProject) => {
+const Index = ({ name, image }: CardProject) => {
 	return (
 		<article className={styles.defaultProjectCard}>
 			<div className={styles.hoverInfo}>
-				<div className={styles.name}>THavana Hammock Chair</div>
+				<div className={styles.name}>{name}</div>
 				<PreviewButton />
 			</div>
 			<img src={image} alt="" />
