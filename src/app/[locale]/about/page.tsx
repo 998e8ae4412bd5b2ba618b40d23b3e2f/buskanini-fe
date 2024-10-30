a1d5abacf60327f6d23b156e09857a79cabd84a2
ihorvyt <ihorvyt@gmail.com>
2024-10-30 19:37:57 +0200

    made mobile version and refactor all scss

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,58 +1,72 @@
 import Header from "@/app/components/header";
 import styles from "./about.module.scss";
+import BlackArrow from "../../../../public/svg/blackArrow.svg";
+import HaveQuestion from "@/app/components/haveQuestion";
 
 const Page = () => {
   return (
     <>
       <Header />
-      <section className={styles.aboutUs}>
-        <div className={styles.aboutUsText}>
-          <h3>Про нас</h3>
-          <p>
-            У Buskanini ми спеціалізуємося на створенні високодеталізованих,
-            реалістичних 3D-моделей для дизайну інтер'єру та меблів. Наша
-            команда фокусується на мінімалістичному та ергономічному дизайні,
-            пропонуючи високоякісні візуалізації, які оживляють простір.
-          </p>
-          <p>
-            Використовуючи передові інструменти, такі як Autodesk 3ds Max та
-            <br />
-            Corona Render, ми гарантуємо, що кожна деталь буде виконана з
-            точністю та артистизмом. <br /> Незалежно від того, чи є ви
-            архітектором, дизайнером або ентузіастом 3D, наші моделі піднесуть
-            ваші проекти та забезпечать приголомшливі результати для
-            візуалізації або виробництва.
-          </p>
+      <section className={`${styles.aboutUs} ${styles.aboutSectionStyles}`}>
+        <div className={styles.content}>
+          <div className={styles.aboutUsText}>
+            <h1>Про нас</h1>
+            <p>
+              У Buskanini ми спеціалізуємося на створенні високодеталізованих,
+              реалістичних 3D-моделей для дизайну інтер'єру та меблів. Наша
+              команда фокусується на мінімалістичному та ергономічному дизайні,
+              пропонуючи високоякісні візуалізації, які оживляють простір.
+            </p>
+            <p>
+              Використовуючи передові інструменти, такі як Autodesk 3ds Max та
+              <br/>
+              Corona Render, ми гарантуємо, що кожна деталь буде виконана з
+              точністю та артистизмом. <br/> Незалежно від того, чи є ви
+              архітектором, дизайнером або ентузіастом 3D, наші моделі піднесуть
+              ваші проекти та забезпечать приголомшливі результати для
+              візуалізації або виробництва.
+            </p>
+          </div>
+          <div
+              className={styles.aboutUsImage}
+              style={{backgroundImage: `url(/images/about/chair.webp)`}}
+          />
         </div>
-        <img src="" alt="" />
       </section>
-      <div className={styles.aboutBuskaniniN}>
-        <h1>bushkanini</h1>
+
+      <div className={`${styles.aboutBuskanini}`}>
+        <span>bushkanini</span>
       </div>
-      <section className={styles.aboutMe}>
-        <div className={styles.aboutMeText}>
-          <h3>Про мене</h3>
-          <p>
-            Привіт! Я є творчою силою компанії Buskanini, де я поєдную свою
-            любов до дизайну з моїм досвідом у 3D-моделюванні. Мені подобається
-            створювати мінімалістичні та ергономічні меблі, які розповідають
-            історію та покращують будь-який простір. Кожна модель, яку я
-            створюю, є відображенням моєї пристрасті до деталей і якості,
-            використовуючи першокласні інструменти, такі як Autodesk 3ds Max.
-          </p>
-          <p>
-            Я завжди досліджую свіжі ідеї та тенденції, щоб просунути своє
-            <br />
-            ремесло далі. Приєднуйтесь до мене в цій захоплюючій подорожі
-            <br />
-            перетворення уяви в приголомшливу візуальну реальність!
-          </p>
-        </div>
-        <div className={styles.aboutMeInfo}>
-          <img src="" alt="" />
-          <div className={styles.aboutMeInfoText}>
-            <h3>Mykola Bushkaniuk</h3>
-            <p>Co-fouder</p>
+
+      <section className={`${styles.aboutMe} ${styles.aboutSectionStyles}`}>
+        <div className={styles.content}>
+          <div className={styles.aboutMeTexts}>
+            <h2>Про мене</h2>
+            <p>
+              Привіт! Я є творчою силою компанії Buskanini, де я поєдную свою
+              любов до дизайну з моїм досвідом у 3D-моделюванні. <br/> Мені подобається
+              створювати мінімалістичні та ергономічні меблі, які розповідають
+              історію та покращують будь-який простір.<br/> Кожна модель, яку я
+              створюю, є відображенням моєї пристрасті до деталей і якості,
+              використовуючи першокласні інструменти, такі як Autodesk 3ds Max.
+            </p>
+            <p>
+              Я завжди досліджую свіжі ідеї та тенденції, щоб просунути своє
+              <br/>
+              ремесло далі. Приєднуйтесь до мене в цій захоплюючій подорожі
+              <br/>
+              перетворення уяви в приголомшливу візуальну реальність!
+            </p>
+          </div>
+
+          <div className={styles.aboutMeInfo}>
+            <div className={styles.aboutMeInfoImage}
+                 style={{backgroundImage: `url(/images/about/MykolaBuskaniuk.webp)`}}
+            />
+            <div className={styles.aboutMeInfoText}>
+              <h3>Mykola Bushkaniuk</h3>
+              <p>Co-fouder</p>
+            </div>
           </div>
         </div>
       </section>
@@ -60,7 +74,7 @@ const Page = () => {
       <section className={styles.aboutProjects}>
         <div className={styles.aboutProjectsWrap}>
           <div className={styles.aboutProjectsProject}>
-            <h3 id="projects-heading">
+            <h3>
               50+ <br />
               Проєктів
             </h3>
@@ -70,7 +84,9 @@ const Page = () => {
               Переглянути проєкти
             </a>
             <button className={styles.arrowBtn}>
-              <img src="" alt="Дізнатися більше" />
+              <div className={styles.arrowIcon}>
+                <BlackArrow/>
+              </div>
             </button>
           </div>
         </div>
@@ -85,13 +101,9 @@ const Page = () => {
         </div>
       </section>
 
-      <section className={styles.aboutContact}>
-        <h2 className={styles.aboutContactHeading}>Маєте запитання?</h2>
-        <p className={styles.aboutContactText}>
-          Пишіть нам у будь-який час — ми завжди на зв'язку, щоб допомогти
-          створити ваш ідеальний простір.
-        </p>
-        <button className={styles.aboutContactButton}>Написати</button>
+
+      <section className={styles.askSection}>
+        <HaveQuestion/>
       </section>
     </>
   );
